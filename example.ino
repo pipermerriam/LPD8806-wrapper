a764fde3c91e0a8fdaec267387257400420474d5
Aaron Merriam <amerriam@fusionbox.com>
2012-05-01 20:36:25 -0600

    updated example code

--- example.ino
+++ example.ino
@@ -1,52 +1,58 @@
 #include "panel.h"
 #include "SPI.h"
 
-#define DATA_PIN_A 2
-#define CLOCK_PIN_A 3
+#define DATA_PIN_A 0
+#define CLOCK_PIN_A 1
 #define DATA_PIN_B 4
 #define CLOCK_PIN_B 5
-#define DATA_PIN_C 6
-#define CLOCK_PIN_C 7
+#define DATA_PIN_C 2
+#define CLOCK_PIN_C 3
 #define DATA_PIN_D 6
 #define CLOCK_PIN_D 7
 
-#define COLUMN_COUNT 2
-#define ROW_COUNT 78
+#define COLUMN_COUNT 6
+#define ROW_COUNT 26
 
 uint8_t STRIPES[6] = {5, 4, 4, 4, 4, 5};
 
-LPD8806 * strip_a = new LPD8806(156, DATA_PIN_A, CLOCK_PIN_A);
-StripWrapper wrapper_a(ROW_COUNT, COLUMN_COUNT, strip_a);
-LPD8806 * strip_b = new LPD8806(208, DATA_PIN_B, CLOCK_PIN_B);
-StripWrapper wrapper_b(COLUMN_COUNT, ROW_COUNT, strip_b);
-LPD8806 * strip_c = new LPD8806(182, DATA_PIN_C, CLOCK_PIN_C);
-StripWrapper wrapper_c(COLUMN_COUNT, 7, strip_c);
-LPD8806 * strip_d = new LPD8806(156, DATA_PIN_D, CLOCK_PIN_D);
-StripWrapper wrapper_d(COLUMN_COUNT, ROW_COUNT, strip_d);
+LPD8806 * strip_a = new LPD8806(104, DATA_PIN_A, CLOCK_PIN_A);
+StripWrapper wrapper_a(4, ROW_COUNT, strip_a);
+LPD8806 * strip_b = new LPD8806(52, DATA_PIN_B, CLOCK_PIN_B);
+StripWrapper wrapper_b(2, ROW_COUNT, strip_b);
+LPD8806 * strip_c = new LPD8806(78, DATA_PIN_C, CLOCK_PIN_C);
+StripWrapper wrapper_c(3, ROW_COUNT, strip_c);
+LPD8806 * strip_d = new LPD8806(78, DATA_PIN_D, CLOCK_PIN_D);
+StripWrapper wrapper_d(3, ROW_COUNT, strip_d);
 
-StripWrapper wrappers[] = {wrapper_a, wrapper_c, wrapper_d};
+StripWrapper wrappers[] = {wrapper_a, wrapper_b, wrapper_c, wrapper_d};
+//StripWrapper wrappers[] = {wrapper_a, wrapper_c, wrapper_d};
 
 Panel panel(4, wrappers);
+//Panel panel(1, wrappers);
 
 // OTHER COLORS
-//#define ALL_OFF 0x000000
-uint32_t ALL_OFF = panel.Color(0, 0, 0);
-//#define ALL_ON 0x7f7f7f
-uint32_t ALL_ON = panel.Color(127, 127, 127);
+#define ALL_OFF 0x000000
+//uint32_t ALL_OFF = panel.Color(0, 0, 0);
+#define ALL_ON 0x7f7f7f
+//uint32_t ALL_ON = panel.Color(127, 127, 127);
 
 // PRIMARY COLORS
-//#define RED 0x7f0000
-uint32_t RED = panel.Color(127, 0, 0);
-//#define ORANGE 0x7f2000
-uint32_t ORANGE = panel.Color(127, 32, 0); // Could use some tweaking
-//#define YELLOW 0x7f7f00
-uint32_t YELLOW = panel.Color(127, 127, 0);
-//#define GREEN 0x007f00
-uint32_t GREEN = panel.Color(0, 127, 0);
-//#define BLUE 0x00007f
-uint32_t BLUE = panel.Color(0, 0, 127);
-//#define PURPLE 0x40107f
-uint32_t PURPLE = panel.Color(64, 16, 100); // Could use some tweaking
+// color order is GRB, not RGB
+//uint32_t RED = panel.Color(127, 0, 0);
+#define RED 0x00f700
+#define ORANGE 0x207f00
+#define YELLOW 0x7f7f00
+#define GREEN 0x007f00
+//uint32_t ORANGE = panel.Color(127, 32, 0); // Could use some tweaking
+//uint32_t YELLOW = panel.Color(127, 127, 0);
+#define GREEN 0x7f0000
+//uint32_t GREEN = panel.Color(0, 127, 0);
+#define BLUE 0x00007f
+//uint32_t BLUE = panel.Color(0, 0, 127);
+#define TEAL 0x7f007f
+#define VIOLET 0x007f7f
+#define PURPLE 0x10407f
+//uint32_t PURPLE = panel.Color(64, 16, 100); // Could use some tweaking
 
 // PIMARY COLORED RAINBOW
 prog_uint32_t RAINBOW[] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
@@ -57,11 +63,73 @@ void setup() {
 
 
 void loop() {
-  fill_rainbow();
-  hard_rainbow();
-  multi_rainbow();
-  rainbow();
-  knight_rider();
+  //fill_rainbow();
+  //hard_rainbow();
+  //multi_rainbow();
+  //rainbow();
+  //knight_rider();
+  
+  //panel.setPixelColor(0, 6, RED);
+  //panel.setPixelColor(6, 6, BLUE);
+  //panel.setPixelColor(6, 0, GREEN);
+
+  //panel.setRowColor(4, TEAL);
+
+  //panel.setColumnColor(8, YELLOW);
+  //panel.show();
+
+  //panel.setPixelColor(2, 8, YELLOW);
+  //panel.setPixelColor(8, 2, YELLOW);
+  //panel.show();
+  //delay(100000);
+
+  //wrapper_a.setPixelColor(0, 2, BLUE);
+  //wrapper_a.setPixelColor(2, 0, RED);
+  //wrapper_a.setRowColor(4, BLUE);
+  //wrapper_a.setColumnColor(1, PURPLE);
+  //wrapper_a.show();
+
+  //wrapper_b.setPixelColor(1, 0, ORANGE);
+  //wrapper_b.setPixelColor(0, 1, GREEN);
+  //wrapper_b.setRowColor(5, RED);
+  //wrapper_b.setColumnColor(1, TEAL);
+  //wrapper_b.show();
+
+  //wrapper_c.setPixelColor(1, 0, RED);
+  //wrapper_c.setPixelColor(0, 1, YELLOW);
+  //wrapper_c.setRowColor(6, GREEN);
+  //wrapper_c.setColumnColor(1, ALL_ON);
+  //wrapper_c.show();
+
+  //wrapper_d.setPixelColor(1, 0, PURPLE);
+  //wrapper_d.setPixelColor(0, 1, ALL_ON);
+  //wrapper_d.setRowColor(7, ORANGE);
+  //wrapper_d.setColumnColor(1, RED);
+  //wrapper_d.show();
+  //delay(10000000);
+
+  pixel_test(ALL_ON);
+  pixel_test(RED);
+  pixel_test(ORANGE);
+  pixel_test(YELLOW);
+  pixel_test(GREEN);
+  pixel_test(BLUE);
+  pixel_test(TEAL);
+  pixel_test(VIOLET);
+  pixel_test(PURPLE);
+  pixel_test(ALL_OFF);
+}
+
+void pixel_test(uint32_t color)
+{
+  for (uint8_t y=0; y < 26; y++)
+  {
+    for (uint8_t x=0; x < 12 ; x++)
+    {
+      panel.setPixelColor(x, y, color);
+      panel.show();
+    }
+  }
 }
 
 void knight_rider()
@@ -110,18 +178,6 @@ void fill_rainbow()
     }
     panel.setRowColor(i - 1, color);
     panel.show();
-  }
-}
-
-void pixel_test(uint32_t color)
-{
-  for(uint8_t y=0; y < 78; y++)
-  {
-    for(uint8_t x=0; x < 2; x++)
-    {
-      panel.setPixelColor(x, y, color);
-      panel.show();
-    }
   }
 }
 
